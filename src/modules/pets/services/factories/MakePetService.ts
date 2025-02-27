0a38848e7d566073d1d683b9adb70f3d5a6fca13
Gustavo <gustavoleandrogorges@gmail.com>
2025-02-27 15:53:53 -0300

    style: reorder import statements for improved readability and consistency

--- src/modules/pets/services/factories/MakePetService.ts
+++ src/modules/pets/services/factories/MakePetService.ts
@@ -1,14 +1,14 @@
-import { PrismaClient } from "@prisma/client"
-import { PetService } from "@/modules/pets"
-import { PetRepository } from "@/modules/pets"
-import { OngRepository } from "@/modules/ongs"
-import { UserRepository } from "@/modules/users"
+import { OngRepository } from "@/modules/ongs";
+import { PetRepository, PetService } from "@/modules/pets";
+import { UserRepository } from "@/modules/users";
+import { PrismaClient } from "@prisma/client";
 
 export function MakePetService() {
-    const prisma = new PrismaClient()
+	const prisma = new PrismaClient();
 
-    return new PetService(
-        new PetRepository(prisma),
-        new OngRepository(prisma),
-        new UserRepository(prisma))
+	return new PetService(
+		new PetRepository(prisma),
+		new OngRepository(prisma),
+		new UserRepository(prisma),
+	);
 }

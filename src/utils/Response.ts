8c52e5b03d9f71928929511f1c2536162a78924c
Gustavo <gustavoleandrogorges@gmail.com>
2025-02-27 15:51:43 -0300

    refactor: update Response class to improve type definitions for data and error properties

--- src/utils/Response.ts
+++ src/utils/Response.ts
@@ -1,10 +1,10 @@
 export class Response {
     status: number;
     message: string;
-    data: any;
-    error?: any;
+    data: object | null;
+    error?: object | string | null;
     
-    constructor(status: number, message: string, data: any, error?: any) {
+    constructor(status: number, message: string, data: object | null, error?: object | string | null) {
         this.status = status;
         this.message = message;
         this.data = data;

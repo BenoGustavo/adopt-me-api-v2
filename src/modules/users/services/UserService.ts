6acbdada10f3a52e519b7c331b9e7c6e94813b2d
Gustavo <gustavoleandrogorges@gmail.com>
2025-02-27 16:16:44 -0300

    style: update Prettier parser to TypeScript and format UserService for consistency

--- src/modules/users/services/UserService.ts
+++ src/modules/users/services/UserService.ts
@@ -1,36 +1,39 @@
+import { env } from "@/env";
+import { PasswordDontMatchError, UserAlreadyExistsError } from "@/errors";
 import { UserInvalidCredentialsError } from "@/errors/UserInvalidCredentialsError";
 import { UserRepository } from "@/modules/users";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { env } from "@/env";
 import { CreateUserDtoType } from "../dtos/UserDTO";
-import { PasswordDontMatchError } from "@/errors";
-import { UserAlreadyExistsError } from "@/errors";
-
 
 export class UserService {
-  constructor(private userRepository: UserRepository) {}
+	constructor(private userRepository: UserRepository) {}
 
-  async create(userData:CreateUserDtoType ) {
-    const user = await this.userRepository.findByEmail(userData.email);
-    if (user) {
-      throw new UserAlreadyExistsError();
-    }
+	async create(userData: CreateUserDtoType) {
+		const user = await this.userRepository.findByEmail(userData.email);
+		if (user) {
+			throw new UserAlreadyExistsError();
+		}
 
-    if (userData.password !== userData.confirmPassword) {
-      throw new PasswordDontMatchError();
-    }
+		if (userData.password !== userData.confirmPassword) {
+			throw new PasswordDontMatchError();
+		}
 
-    const hashedPassword = await bcrypt.hash(userData.password, 10);
-    return (await this.userRepository.create({ ...userData, password: hashedPassword }));
-  }
+		const hashedPassword = await bcrypt.hash(userData.password, 10);
+		return await this.userRepository.create({
+			...userData,
+			password: hashedPassword,
+		});
+	}
 
-  async authenticate(email: string, password: string) {
-    const user = await this.userRepository.findByEmail(email);
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-      throw new UserInvalidCredentialsError("Invalid credentials");
-    }
-    const token = jwt.sign({ id: user.id }, env.JWT_SECRET!, { expiresIn: "1d" });
-    return { ...user, token, password: undefined };
-  }
+	async authenticate(email: string, password: string) {
+		const user = await this.userRepository.findByEmail(email);
+		if (!user || !(await bcrypt.compare(password, user.password))) {
+			throw new UserInvalidCredentialsError("Invalid credentials");
+		}
+		const token = jwt.sign({ id: user.id }, env.JWT_SECRET!, {
+			expiresIn: "1d",
+		});
+		return { ...user, token, password: undefined };
+	}
 }

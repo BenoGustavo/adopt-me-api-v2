98a6d0319ee652610ea89982edd6be8f82b3c408
Gustavo <gustavoleandrogorges@gmail.com>
2025-02-27 15:51:34 -0300

    chore: update ESLint and Prettier configurations for improved code quality

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,28 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import prettierPlugin from "eslint-plugin-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-	{ files: ["**/*.{js,mjs,cjs,ts}"] },
-	{ languageOptions: { globals: globals.node } },
-	pluginJs.configs.recommended,
-	...tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    ignores: [
+      "node_modules",
+      "dist",
+      "*.spec.ts",
+      "*.spec.js",
+      "*.test.ts",
+      "*.test.js"
+    ],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off"
+    },
+    plugins: {
+      prettier: prettierPlugin
+    }
+  },
+  { languageOptions: { globals: globals.node, parser: "@typescript-eslint/parser" } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended
 ];
